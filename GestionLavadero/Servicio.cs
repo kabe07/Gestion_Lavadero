6edd7a79ec85efbb2c71f8641795a64b332e656e
Martin Fernandez <martinfernandezminni@gmail.com>
2026-02-05 22:02:38 -0300

    creando la clase servicio y el enum de los tipos de servicio}

---
+++ GestionLavadero/Servicio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionLavadero
+{
+    internal class Servicio
+    {
+        private TipoServicio tipo;
+        private Dictionary<TipoVehiculo, double> precios;
+        public Servicio(TipoServicio tipo, Dictionary<TipoVehiculo,double> precios)
+        {
+            this.tipo = tipo;
+            this.precios = precios;
+
+        }
+        public double ObtenerPrecioBase(TipoVehiculo tipo)
+        {
+            return precios[tipo];
+        }
+    
+    }
+}
---
+++ GestionLavadero/TipoServicio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionLavadero
+{
+    public enum TipoServicio
+    {
+        Lavado_comun,
+        Medio_lavado,
+        Encerado,
+        Lavado_motor,
+        Limpieza_tapizados,
+
+    }
+}
