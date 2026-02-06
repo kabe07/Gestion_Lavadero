7eb3ad996352881ecdba9d735d82cacd68b2be6d
Martin Fernandez <martinfernandezminni@gmail.com>
2026-02-06 08:47:09 -0300

    Generando la clase turno

---
+++ GestionLavadero/Turno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionLavadero
+{
+    internal class Turno
+    {
+        private DateTime fechayhora;
+        private Vehiculo vehiculo;
+        private Servicio servicio;
+        private double extra;
+        private double descuento;
+        public Turno(DateTime fecha, Vehiculo vehiculo, Servicio servicio)
+        {
+            fechayhora = fecha;
+            this.vehiculo = vehiculo;
+            this.servicio = servicio;
+        }
+        public double CalcularPrecioFinal()
+        {
+            return servicio.ObtenerPrecioBase(vehiculo.Tipo);
+        }
+        public string ToString()
+        {
+            return $"Servicio fecha:{fechayhora} vehiculo:{vehiculo.ToString()} servicio:{servicio.ToString()}";
+        }
+    }
+}
