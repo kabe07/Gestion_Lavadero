39be2c46be0c38c1d4c892db49543d88a0d40037
Martin Fernandez <martinfernandezminni@gmail.com>
2026-02-05 21:41:32 -0300

    Cree la clase vehiculo

---
+++ GestionLavadero/Vehiculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionLavadero
+{
+    internal class Vehiculo
+    {
+        private string marca;
+        private string modelo;
+        public TipoVehiculo tipo;
+        public Vehiculo(string marca, string modelo, TipoVehiculo tipo)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.tipo = tipo;
+        }
+        public string ToString()
+        {
+            return $"Vehiculo marca:{marca}-modelo:{modelo}-tipo:{tipo}";
+        }
+    }
+}
