7d9a0d9aac6d57b9c511cf8add558d29b741b021
Martin Fernandez <martinfernandezminni@gmail.com>
2026-02-05 21:32:54 -0300

    Comenzando proyecto de gestion de lavadero

---
+++ GestionLavadero/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionLavadero
+{
+    internal class Cliente
+    {
+        private string nombre;
+        private int dni;
+        public string Nombre { get; set; }
+        public int Dni { get; set; }
+        private List<Vehiculo> vehiculos;
+        public Cliente(string nombre, int dni)
+        {
+            Nombre = nombre;
+            Dni = dni;
+            vehiculos = new List<Vehiculo>();
+
+        }
+        public string ToString()
+        {
+            return $"Cliente nombre:{Nombre}-Dni:{Dni}";
+        }
+
+    }
+}
