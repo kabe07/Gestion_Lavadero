1ae67beaed85900d4ca85aba2ad4e48b9c0c3fdc
Martin Fernandez <martinfernandezminni@gmail.com>
2026-02-06 11:06:23 -0300

    Cargo en el load los clientes que se encuentren en la list de Sistema y tomo los clientes que se agreguen desde ventana modal y los cargo en la lista

--- GestionLavadero/Form1.Designer.cs
+++ GestionLavadero/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbclientes = new System.Windows.Forms.ComboBox();
             this.cbservicio = new System.Windows.Forms.ComboBox();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -53,6 +53,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Agregar Cliente";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -63,13 +64,14 @@
             this.button2.Text = "Agregar Vehiculo";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // comboBox1
+            // cbclientes
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(112, 38);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 2;
+            this.cbclientes.FormattingEnabled = true;
+            this.cbclientes.Location = new System.Drawing.Point(112, 38);
+            this.cbclientes.Name = "cbclientes";
+            this.cbclientes.Size = new System.Drawing.Size(121, 21);
+            this.cbclientes.TabIndex = 2;
+            this.cbclientes.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // cbservicio
             // 
@@ -190,7 +192,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.cbservicio);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbclientes);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -205,7 +207,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbclientes;
         private System.Windows.Forms.ComboBox cbservicio;
         private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.Label label1;
--- GestionLavadero/Form1.cs
+++ GestionLavadero/Form1.cs
@@ -17,17 +17,38 @@ namespace GestionLavadero
             InitializeComponent();
         }
         SistemaLavadero sistema = new SistemaLavadero();
+        
         private void Form1_Load(object sender, EventArgs e)
         {
             sistema.InicializarServicios();
             List<Servicio> ser = sistema.ObtenerServicios();
             cbservicio.DataSource = ser;
-
+            List<Cliente> clientes = sistema.ObtenerClientes();
+            cbclientes.DataSource = clientes;
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VentanaCliente ventana2 = new VentanaCliente();
+            if(ventana2.ShowDialog()==DialogResult.OK)
+            {
+                string nombre = ventana2.tbnombre.Text;
+                int dni = Convert.ToInt32(ventana2.tbdni.Text);
+                sistema.CrearClientes(nombre, dni);
+                cbclientes.DataSource = null;
+                List<Cliente> clientes = sistema.ObtenerClientes();
+                cbclientes.DataSource = clientes;
+            }
         }
     }
 }
